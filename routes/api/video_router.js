af8b1b76ded753bed6d5a4caaf36d3955daaee3a
bensheng.zhang 张本升 <bensheng.zhang@qiyuesuo.com>
2019-06-09 19:08:15 +0800

    update

--- routes/api/video_router.js
+++ routes/api/video_router.js
@@ -3,6 +3,6 @@ const router = new Router();
 const video_controller = require('./../../app/controllers/video_controller');
 
 router.get('/videos', video_controller.getVideos);
-router.post('/addvideo', video_controller.addvideo);
+router.post('/addvideo', video_controller.addVideo);
 
 module.exports = router;
